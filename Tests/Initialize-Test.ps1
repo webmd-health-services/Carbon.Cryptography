962f7d1622b6a712dcf098d21e120e002b9f7d93
Aaron Jensen <ajensen@webmd.net>
2021-01-29 13:51:27 -0800

    Use our own environment variable to determine if we're running under a build server or not.

--- Tests/Initialize-Test.ps1
+++ Tests/Initialize-Test.ps1
@@ -48,7 +48,7 @@ try
         if( $module )
         {
             # Don't constantly reload modules on the build server.
-            if( (Test-Path -Path 'env:WHS_CI') -and $module.Path.StartsWith($modulePath) )
+            if( (Test-Path -Path 'env:CARBON_CI') -and $module.Path.StartsWith($modulePath) )
             {
                 continue
             }
